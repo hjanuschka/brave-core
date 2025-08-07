0800033d2fad6fa28e41e811b6c96db4d41437cf
Claudio DeSouza <cdesouza@brave.com>
2025-08-07 15:54:37 +0100

    [cr141] `kBookmarkTriggerForPrerender2` deleted
    Upstream introduced a different feature flag to disable it:
    kBookmarkTriggerForPrerender2KillSwitch.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/83c9c42e855f2a9a762ee9416f7351f6bad3d1ec
    
    commit 83c9c42e855f2a9a762ee9416f7351f6bad3d1ec
    Author: Robert Lin <robertlin@chromium.org>
    Date:   Tue Aug 5 18:36:55 2025 -0700
    
        Clean up features::kBookmarkTriggerForPrerender2 and introduce features::kBookmarkTriggerForPrerender2KillSwitch
    
        This CL does
        1. Remove `features::kBookmarkTriggerForPrerender2` as the
        feature has been enabled by default. And it is no longer needed.
        2. With removal of the feature flag, it is still desired to have a kill
        switch to have a way to disable in case of emergency.
        `features::kBookmarkTriggerForPrerender2KillSwitch` is introduced in
         `BookmarkBarPreloadPipeline::StartPrerender` to do so.
    
        Bug: 40259793
        Change-Id: I2af922338277e25aa3a868a8139da59929dd624d
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6818916
        Commit-Queue: Huanpo Lin <robertlin@chromium.org>
        Reviewed-by: Marc Treib <treib@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1497309}

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -130,7 +130,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
       &feature_engagement::kIPHGMCCastStartStopFeature,
       &feature_engagement::kIPHPasswordsManagementBubbleAfterSaveFeature,
 #endif
-      &features::kBookmarkTriggerForPrerender2,
       &features::kChromeStructuredMetrics,
       &features::kCookieDeprecationFacilitatedTesting,
 #if !BUILDFLAG(IS_ANDROID)
@@ -253,6 +252,7 @@ TEST(FeatureDefaultsTest, EnabledFeatures) {
       &blink::features::kReducedReferrerGranularity,
       &blink::features::kReduceUserAgentMinorVersion,
       &blink::features::kUACHOverrideBlank,
+      &features::kBookmarkTriggerForPrerender2KillSwitch,
       &features::kCertificateTransparencyAskBeforeEnabling,
       &features::kDesktopPWAsTabStripSettings,
 #if !BUILDFLAG(IS_ANDROID)
